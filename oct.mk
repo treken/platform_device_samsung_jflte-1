a6fd1c5e72ec2dcfafd10566f5907dc807315275
Donald Hoskins <grommish@gmail.com>
2014-10-18 20:49:48 -0400

    Update oct.mk

--- oct.mk
+++ oct.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/samsung/jflte/full_jflte.mk)
 # Enhanced NFC $(call inherit-product, 
-#vendor/oct/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
 # Inherit some common oct stuff.
 $(call inherit-product, vendor/oct/config/common_phone.mk)
 PRODUCT_NAME := oct_jflte
